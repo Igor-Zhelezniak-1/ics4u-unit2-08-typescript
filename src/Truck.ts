5cab0a10a556af63de6a2ba0eed5539f97e954fb
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2022-12-21 21:46:33 -0500

    -

---
+++ src/Truck.ts
@@ -0,0 +1,50 @@
+/**
+ * Truck
+ * By: Igor
+ * Version: 1.0
+ * Since:   2022-10-21
+ */
+
+import Vehicle from './Vehicle'
+
+class Truck extends Vehicle {
+  private licensePlate: string
+
+  constructor(
+    licensePlate: string,
+    color: string,
+    maxSpeed: number,
+    numberTires: number
+  ) {
+    super(color, maxSpeed, numberTires)
+    this.licensePlate = licensePlate
+  }
+
+  // status
+  status(): void {
+    super.status()
+    console.log(`    → LicensePlate: ${this.licensePlate}`)
+  }
+
+  // getter for licensPlate
+  getLicensePlate(): string {
+    return this.licensePlate
+  }
+
+  // setter for licence plate
+  setLicensePlate(newLicensePlate: string): void {
+    this.licensePlate = newLicensePlate
+  }
+
+  // method for airBrake
+  airBrake(airPressure: number): void {
+    super.setSpeed(super.getSpeed() - airPressure / 2)
+
+    // reality Check
+    if (super.getSpeed() < 0) {
+      super.setSpeed(0)
+    }
+  }
+}
+
+export = Truck
